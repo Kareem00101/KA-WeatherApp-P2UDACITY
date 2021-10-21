376c28e79b6608874e06376fe3cd05a8a6c86794
Kareem00101 <89580954+Kareem00101@users.noreply.github.com>
2021-10-21 23:23:17 +0200

    Added API creds

--- website/app.js
+++ website/app.js
@@ -3,3 +3,7 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// API credentials and url 
+const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=8d143f4de42234bc4eb01542de2d28ad';
